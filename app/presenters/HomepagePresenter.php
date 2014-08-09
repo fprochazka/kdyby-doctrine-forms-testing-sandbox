840561f49e46c5a8c57f7265505f19b08c71a174
Tomas Votruba <info@tomasvotruba.cz>
2014-08-09 03:10:48 +0200

    SimpleForm related changes in Homepage

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,10 +2,26 @@
 
 namespace App\Presenters;
 
+use App\Components\SimpleForm;
 use Nette;
 
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+	/**
+	 * @var SimpleForm\IControlFactory
+	 * @inject
+	 */
+	public $simpleFormFactory;
+
+
+	/**
+	 * @return SimpleForm\Control
+	 */
+	protected function createComponentSimpleForm()
+	{
+		$control = $this->simpleFormFactory->create();
+		return $control;
+	}
 
 }
